0bd64a509c5ffc396b9d36eb8c3e79140f92e0e1
Kjetil Torvund Minde <kjetil.torvund.minde@ngn.no>
2022-06-20 23:17:19 +0200

    Fixed health endpoint.

--- src/main/java/no/ding/pk/web/HomeController.java
+++ src/main/java/no/ding/pk/web/HomeController.java
@@ -1,11 +1,13 @@
 package no.ding.pk.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/health")
     public String healthCheck() {
         return "OK!";
     }
