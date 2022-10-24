fbaa8990d67d2c89d31def8c5bd11949e7dcf3a8
Kjetil Torvund Minde <kjetil.torvund.minde@ngn.no>
2022-10-24 23:48:12 +0200

    Added cors origin allowance.

--- src/main/java/no/ding/pk/web/controllers/CustomerController.java
+++ src/main/java/no/ding/pk/web/controllers/CustomerController.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import no.ding.pk.service.CustomerServiceImpl;
 import no.ding.pk.web.dto.CustomerDTO;
 import no.ding.pk.web.enums.SapCustomerField;
 
+@CrossOrigin(origins = "${allowed.cors.url}")
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
