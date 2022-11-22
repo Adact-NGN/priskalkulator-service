f323b757b96b98d062cd983b9950e400b6a533ee
Kjetil Torvund Minde <kjetil.torvund.minde@ngn.no>
2022-11-22 09:33:40 +0100

    Added swagger ui doc

--- src/main/java/no/ding/pk/App.java
+++ src/main/java/no/ding/pk/App.java
@@ -3,6 +3,12 @@ package no.ding.pk;
 import java.net.MalformedURLException;
 import java.util.List;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.slf4j.Logger;
@@ -29,6 +35,7 @@ import no.ding.pk.domain.User;
 import no.ding.pk.web.dto.AdUserDTO;
 
 @EnableScheduling
+@EnableSwagger2
 @SpringBootApplication
 @PropertySource({
     "classpath:application.properties", 
@@ -108,4 +115,11 @@ public class App implements WebMvcConfigurer {
 	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+        .select().apis(RequestHandlerSelectors.basePackage("no.ding.pk.web.controllers"))
+        .paths(PathSelectors.any()).build();
+    }
 }
