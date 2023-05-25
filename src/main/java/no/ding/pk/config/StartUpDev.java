1453d81c0136af50b6495a65ced5ed4960a65bfa
Kjetil Torvund Minde <kjetil.torvund.minde@ngn.no>
2023-05-25 11:01:36 +0200

    Simplified startup dev code.

--- src/main/java/no/ding/pk/config/StartUpDev.java
+++ src/main/java/no/ding/pk/config/StartUpDev.java
@@ -2,13 +2,7 @@ package no.ding.pk.config;
 
 import no.ding.pk.domain.SalesRole;
 import no.ding.pk.domain.User;
-import no.ding.pk.domain.offer.Material;
-import no.ding.pk.domain.offer.MaterialPrice;
-import no.ding.pk.domain.offer.PriceOffer;
-import no.ding.pk.domain.offer.PriceOfferTerms;
-import no.ding.pk.domain.offer.PriceRow;
-import no.ding.pk.domain.offer.SalesOffice;
-import no.ding.pk.domain.offer.Zone;
+import no.ding.pk.domain.offer.*;
 import no.ding.pk.service.SalesRoleService;
 import no.ding.pk.service.UserService;
 import no.ding.pk.service.offer.MaterialService;
@@ -17,7 +11,6 @@ import no.ding.pk.service.sap.StandardPriceService;
 import no.ding.pk.web.dto.sap.MaterialStdPriceDTO;
 import no.ding.pk.web.enums.PriceOfferStatus;
 import no.ding.pk.web.enums.SalesRoleName;
-import no.ding.pk.web.enums.TermsTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +20,6 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Profile("dev")
@@ -63,10 +55,10 @@ public class StartUpDev {
 
                 if(kjetil == null) {
                         kjetil = createAndGetUser("dc804853-6a82-4022-8eb5-244fff724af2", "Kjetil",
-                                "Torvund Minde", "Kjetil Torvund Minde", "100",
-                                "Norsk Gjenvinning", "Larvik", "90135757",
+                                "Torvund Minde", "Kjetil Torvund Minde",
+                                "Larvik", "90135757",
                                 "kjetil.torvund.minde@ngn.no",
-                                "Systemutvikler", 5, 5, null);
+                                "Systemutvikler", 5, null);
                 }
 
                 SalesRole admin = salesRoleService.findSalesRoleByRoleName(SalesRoleName.Superadmin.name());
@@ -81,9 +73,9 @@ public class StartUpDev {
                 if(alex == null) {
 
                         alex = createAndGetUser("e2f1963a-072a-4414-8a0b-6a3aa6988e0c", "Alexander",
-                                        "Brox", "Alexander Brox","100","Norsk Gjenvinning",
+                                        "Brox", "Alexander Brox",
                                 "Oslo", "95838638", "alexander.brox@ngn.no",
-                                "Markedskonsulent", 5, 3, "63874");
+                                "Markedskonsulent", 3, "63874");
                 }
 
                 SalesRole marketConsultant = salesRoleService.findSalesRoleByRoleName(SalesRoleName.Kundeveileder.name());
@@ -120,11 +112,6 @@ public class StartUpDev {
 
                 log.debug("Sales consultant Sales Role: {}", salesConsultant);
 
-                PriceOfferTerms customerTerms = PriceOfferTerms.builder()
-                        .contractTerm(TermsTypes.GeneralTerms.getValue())
-                        .agreementStartDate(new Date())
-                        .build();
-
                 List<MaterialStdPriceDTO> materialDTOs = priceService.getStdPricesForSalesOfficeAndSalesOrg("100", "100", null);
 
                 log.debug("Material DTO list size: {}", materialDTOs.size());
@@ -139,11 +126,11 @@ public class StartUpDev {
 
                 List<String> zone0MaterialNumberList = List.of("50201", "50203", "50101", "50102", "50104");
                 List<PriceRow> zone0MaterialList = createPriceRowList(zone0MaterialNumberList, materialDTOs);
-                Zone zone0 = createZone("0000000001", "1601", "FREDRIKSTAD", true, zone0MaterialList);
+                Zone zone0 = createZone("0000000001", true, zone0MaterialList);
 
                 List<String> zone1MaterialNumberList = List.of("50201", "50203", "50101", "50102", "50104");
                 List<PriceRow> zone1MaterialList = createPriceRowList(zone1MaterialNumberList, materialDTOs);
-                Zone zone1 = createZone("0000000002", "1601", "FREDRIKSTAD", false, zone1MaterialList);
+                Zone zone1 = createZone("0000000002", false, zone1MaterialList);
 
                 List<Zone> zoneList = List.of(zone0, zone1);
 
@@ -194,22 +181,22 @@ public class StartUpDev {
                 priceOfferService.save(priceOffer);
         }
 
-        private User createAndGetUser(String adId, String name, String sureName, String fullName, String orgNr,
-                                      String orgName, String associatedPlace, String phoneNumber, String email,
-                                      String jobTitle, int powerOfAttorneyFA, int powerOfAttorneyOA, String resourceNr) {
+        private User createAndGetUser(String adId, String name, String sureName, String fullName,
+                                      String associatedPlace, String phoneNumber, String email,
+                                      String jobTitle, int powerOfAttorneyOA, String resourceNr) {
                 return userService.save(
                         User.builder()
                                 .adId(adId)
                                 .name(name)
                                 .sureName(sureName)
                                 .fullName(fullName)
-                                .orgNr(orgNr)
-                                .orgName(orgName)
+                                .orgNr("100")
+                                .orgName("Norsk Gjenvinning")
                                 .associatedPlace(associatedPlace)
                                 .phoneNumber(phoneNumber)
                                 .email(email)
                                 .jobTitle(jobTitle)
-                                .powerOfAttorneyFA(powerOfAttorneyFA)
+                                .powerOfAttorneyFA(5)
                                 .powerOfAttorneyOA(powerOfAttorneyOA)
                                 .resourceNr(resourceNr)
                                 .build(),
@@ -230,41 +217,37 @@ public class StartUpDev {
         private PriceRow createPriceRow(List<MaterialStdPriceDTO> materialDTOs, String materialNumber) {
                 MaterialStdPriceDTO materialDTO = materialDTOs.stream().filter(obj -> obj.getMaterial().equalsIgnoreCase(materialNumber)).findAny().orElse(null);
                 log.debug("Found materialDTO: {}", materialDTO);
+                assert materialDTO != null;
                 MaterialPrice residualWasteMaterialStdPrice = createMaterialStdPrice(materialNumber, materialDTO.getStandardPrice());
                 Material material = createMaterial(materialNumber, materialDTO, residualWasteMaterialStdPrice);
 
                 material = materialService.save(material);
 
-                return createPriceRow(materialDTO.getStandardPrice(), 0.02, true, materialDTO.getStandardPrice(), 1, 56.0, 1, materialDTO.getStandardPrice(), material);
+                return createPriceRow(materialDTO.getStandardPrice(), materialDTO.getStandardPrice(), materialDTO.getStandardPrice(), material);
         }
 
-        private Zone createZone(String zoneId, String postalCode, String postalName, boolean isStandardZone, List<PriceRow> zoneMaterialList) {
+        private Zone createZone(String zoneId, boolean isStandardZone, List<PriceRow> zoneMaterialList) {
                 return Zone.builder()
                         .zoneId(zoneId)
-                        .postalCode(postalCode)
-                        .postalName(postalName)
+                        .postalCode("1601")
+                        .postalName("FREDRIKSTAD")
                         .isStandardZone(isStandardZone)
                         .priceRows(zoneMaterialList)
                         .build();
         }
 
         private PriceRow createPriceRow(Double customerPrice,
-                                        Double discountPct,
-                                        boolean showPriceInOffer,
                                         Double manualPrice,
-                                        int discountLevel,
-                                        Double discountLevelPrice,
-                                        int amount,
                                         Double priceIncMva,
                                         Material residualWasteMaterial) {
                 return PriceRow.builder()
                         .customerPrice(customerPrice)
-                        .discountPct(discountPct)
-                        .showPriceInOffer(showPriceInOffer)
+                        .discountPct(0.02)
+                        .showPriceInOffer(true)
                         .manualPrice(manualPrice)
-                        .discountLevel(discountLevel)
-                        .discountLevelPrice(discountLevelPrice)
-                        .amount(amount)
+                        .discountLevel(1)
+                        .discountLevelPrice(56.0)
+                        .amount(1)
                         .priceIncMva(priceIncMva)
                         .material(residualWasteMaterial)
                         .build();
