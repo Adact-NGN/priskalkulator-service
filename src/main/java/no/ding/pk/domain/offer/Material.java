54cbf81fb1951bcbf33a556bc700e7851e9a528b
Kjetil Torvund Minde <kjetil.torvund.minde@ngn.no>
2023-06-06 20:47:21 +0200

    Removed unused imports.

--- src/main/java/no/ding/pk/domain/offer/Material.java
+++ src/main/java/no/ding/pk/domain/offer/Material.java
@@ -1,29 +1,11 @@
 package no.ding.pk.domain.offer;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.hibernate.Hibernate;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Setter
 @Getter
