eb6dc64f0e39281e8b54683f2228ed32ffd42226
kjetilatngn <98886750+kjetilatngn@users.noreply.github.com>
2023-09-21 00:58:38 +0200

    Updated PriceOfferTemplateController with more end points. (#51)

--- src/main/java/no/ding/pk/service/offer/PriceOfferTemplateService.java
+++ src/main/java/no/ding/pk/service/offer/PriceOfferTemplateService.java
@@ -2,8 +2,13 @@ package no.ding.pk.service.offer;
 
 import no.ding.pk.domain.offer.PriceOfferTemplate;
 
+import java.util.List;
+
 public interface PriceOfferTemplateService {
 
     PriceOfferTemplate save(PriceOfferTemplate newTemplate);
-    
+
+    List<PriceOfferTemplate> findAll();
+
+    PriceOfferTemplate findById(Long id);
 }
