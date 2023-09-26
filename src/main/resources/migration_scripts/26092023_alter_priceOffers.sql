4429700f19de615d58abeebb175b6007b8d0a573
kjetilatngn <98886750+kjetilatngn@users.noreply.github.com>
2023-09-26 11:05:20 +0200

    Created migration script for priceoffer table. (#61)
    Updated mapping for PriceOffer.

---
+++ src/main/resources/migration_scripts/26092023_alter_priceOffers.sql
@@ -0,0 +1,5 @@
+alter table price_offers
+    add city                   varchar(255)                            null,
+    add postal_number          varchar(255)                            null,
+    add street_address         varchar(255)                            null,
+    add organization_number    varchar(255)                            null;
