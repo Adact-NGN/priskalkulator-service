67082c4dc5d3e992d2ae6a8e208645e6c94fc797
Kjetil Torvund Minde <kjetil.torvund.minde@ngn.no>
2023-09-30 20:16:55 +0200

    Adding access control to api endpoints.

--- src/main/java/no/ding/pk/config/SecurityConfig.java
+++ src/main/java/no/ding/pk/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package no.ding.pk.config;
 
 import com.azure.spring.cloud.autoconfigure.aad.AadResourceServerWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
 @Profile({"default", "dev", "prod"})
 @Configuration
@@ -13,11 +19,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends AadResourceServerWebSecurityConfigurerAdapter {
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String jwkIssuerUri;
+
+    @Autowired
+    private JwtTokenValidator jwtTokenValidator;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeHttpRequests()
                 .anyRequest().permitAll()
-                .and().csrf().disable();
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(this.jwkIssuerUri);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(this.jwkIssuerUri);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, this.jwtTokenValidator);
+        jwtDecoder.setJwtValidator(withAudience);
+        return jwtDecoder;
     }
 }
