61ef9969821379f2d03af200c910b3f71f1f16b6
Kjetil Torvund Minde <kjetil.torvund.minde@ngn.no>
2023-11-07 23:20:48 +0100

    Added routine to get the authorization token.

--- src/main/java/no/ding/pk/service/sap/SapPricingImpl.java
+++ src/main/java/no/ding/pk/service/sap/SapPricingImpl.java
@@ -2,14 +2,22 @@ package no.ding.pk.service.sap;
 
 import no.ding.pk.utils.RequestHeaderUtil;
 import no.ding.pk.utils.SapHttpClient;
+import no.ding.pk.web.dto.sap.pricing.ConditionRecordValidityDTO;
+import no.ding.pk.web.handlers.ErrorRetrievingTokenException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class SapPricingImpl implements SapPricing {
@@ -24,12 +32,47 @@ public class SapPricingImpl implements SapPricing {
         this.sapHttpClient = sapHttpClient;
     }
 
-    private String getToken() {
-        String tokenPath = "/sap/opu/odata/sap/API_SLSPRICINGCONDITIONRECORD_SRV/A_SlsPrcgConditionRecord?$top=1";
+    /**
+     * Add new pricing entity to SAP
+     * <a href="https://sapdev.norskgjenvinning.no/sap(bD1ubyZjPTEwMA==)/bc/bsp/sap/zgw_openapi/index.html?service=API_SLSPRICINGCONDITIONRECORD_SRV&version=0001&repository=&group=#/Betingelsesposter/post_A_SlsPrcgConditionRecord">Add new entity to A_SlsPrcgConditionRecord</a>
+     */
+    public ConditionRecordValidityDTO updateMaterialPricingEntity() {
+        String token = getToken();
 
-        UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromUriString(sapPricingConditionRecordUrl).queryParam("$top", "1");
-        HttpRequest getTokenRequest = HttpRequest.newBuilder()
-                .GET()
-                .headers("X-CSRF-Token", "fetch", HttpHeaders.AUTHORIZATION, RequestHeaderUtil.getBasicAuthenticationHeader(sapUsername, sapPassword)))
+        Map<String, String> headers = Map.of("X-CSRF-Token", token);
+        HttpRequest request = sapHttpClient.createPostRequest(sapPricingConditionRecordUrl, new LinkedMultiValueMap<>(), headers);
+
+        HttpResponse<String> response = sapHttpClient.getResponse(request);
+
+        if(response.statusCode() == HttpStatus.OK.value()) {
+                
+        }
+
+        throw new RuntimeException("Error adding new pricing entity. Service returned with status: " + response.statusCode());
+    }
+
+    private String getToken() {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("$top", "1");
+
+        Map<String, String> headers = Map.of("X-CSRF-Token", "fetch");
+        HttpRequest request = sapHttpClient.createGetRequest(sapPricingConditionRecordUrl, params, headers);
+
+        HttpResponse<String> response = sapHttpClient.getResponse(request);
+
+        if(response.statusCode() == HttpStatus.OK.value()) {
+            log.debug("Got token request successful");
+
+            Optional<String> optionalToken = response.headers().firstValue("x-csrf-token");
+
+            if(optionalToken.isPresent()) {
+                return optionalToken.get();
+            }
+        }
+
+        log.debug("Requesting for Token resulting in bad response: %d", response.statusCode());
+
+        throw new ErrorRetrievingTokenException();
     }
 }
--- src/main/java/no/ding/pk/utils/SapHttpClient.java
+++ src/main/java/no/ding/pk/utils/SapHttpClient.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -84,5 +85,9 @@ public class SapHttpClient {
 
             throw new Error(e.getMessage());
         }
+    }
+
+    public HttpRequest createPostRequest(String sapPricingConditionRecordUrl, LinkedMultiValueMap<Object, Object> objectObjectLinkedMultiValueMap, Map<String, String> headers) {
+        return null;
     }
 }
